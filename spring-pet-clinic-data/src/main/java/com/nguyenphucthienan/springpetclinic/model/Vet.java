807897308ad288418d10deb253ce689ce797566b
Nguyen Phuc Thien An <npta97@gmail.com>
2020-05-20 21:02:54 +0700

    Add Speciality entity. Closes #28

--- spring-pet-clinic-data/src/main/java/com/nguyenphucthienan/springpetclinic/model/Vet.java
+++ spring-pet-clinic-data/src/main/java/com/nguyenphucthienan/springpetclinic/model/Vet.java
@@ -1,4 +1,17 @@
 package com.nguyenphucthienan.springpetclinic.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Vet extends Person {
+
+    Set<Speciality> specialities = new HashSet<>();
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
