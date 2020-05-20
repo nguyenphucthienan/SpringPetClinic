21710db859fa6eb49e69aa388cd10db32356eafa
Nguyen Phuc Thien An <npta97@gmail.com>
2020-05-20 21:59:08 +0700

    Add Speciality service. Closes #31

---
+++ spring-pet-clinic-data/src/main/java/com/nguyenphucthienan/springpetclinic/services/SpecialityService.java
@@ -0,0 +1,7 @@
+package com.nguyenphucthienan.springpetclinic.services;
+
+import com.nguyenphucthienan.springpetclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+
+}
---
+++ spring-pet-clinic-data/src/main/java/com/nguyenphucthienan/springpetclinic/services/map/SpecialityMapService.java
@@ -0,0 +1,10 @@
+package com.nguyenphucthienan.springpetclinic.services.map;
+
+import com.nguyenphucthienan.springpetclinic.model.Speciality;
+import com.nguyenphucthienan.springpetclinic.services.SpecialityService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialityService {
+
+}
