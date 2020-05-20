8e5a296df55ac9540b9ef78d92ffd791dd7010cc
Nguyen Phuc Thien An <npta97@gmail.com>
2020-05-20 20:57:02 +0700

    Add Visit entity. Closes #27

--- spring-pet-clinic-data/src/main/java/com/nguyenphucthienan/springpetclinic/model/Owner.java
+++ spring-pet-clinic-data/src/main/java/com/nguyenphucthienan/springpetclinic/model/Owner.java
@@ -1,4 +1,17 @@
 package com.nguyenphucthienan.springpetclinic.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Owner extends Person {
+
+    private Set<Pet> pets = new HashSet<>();
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
