c4c5e52ae9a1973ff9bfc275cdb6e10d70c2374e
Nguyen Phuc Thien An <npta97@gmail.com>
2020-05-21 12:36:46 +0700

    Update Visit with JPA entity mappings. Closes #38

--- spring-pet-clinic-data/src/main/java/com/nguyenphucthienan/springpetclinic/model/Pet.java
+++ spring-pet-clinic-data/src/main/java/com/nguyenphucthienan/springpetclinic/model/Pet.java
@@ -2,6 +2,8 @@ package com.nguyenphucthienan.springpetclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -20,6 +22,9 @@ public class Pet extends BaseEntity {
     @ManyToOne
     @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
@@ -51,5 +56,13 @@ public class Pet extends BaseEntity {
 
     public void setOwner(Owner owner) {
         this.owner = owner;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 }
