9b26a74cb2c2e3841e25b0e11052ff3402107ff7
Nguyen Phuc Thien An <npta97@gmail.com>
2020-05-21 12:17:12 +0700

    Update Owner with JPA entity mappings. Closes #36

--- spring-pet-clinic-data/src/main/java/com/nguyenphucthienan/springpetclinic/model/Pet.java
+++ spring-pet-clinic-data/src/main/java/com/nguyenphucthienan/springpetclinic/model/Pet.java
@@ -1,12 +1,24 @@
 package com.nguyenphucthienan.springpetclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity {
 
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_type_id")
     private PetType petType;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
 
     public String getName() {
